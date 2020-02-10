e048a5079f080aae57740982cad9f5a6bb560fa7
ushiboy <ushiboy.dev@gmail.com>
2020-02-10 09:51:53 +0900

    fix api interface

--- nmcli/_general.py
+++ nmcli/_general.py
@@ -1,11 +1,11 @@
 from ._system import SystemCommandInterface, SystemCommand
 from .data import General
-from typing import List
+from typing import Optional
 import re
 
 class GeneralControlInterface(object):
 
-    def __call__(self) -> List[General]:
+    def __call__(self) -> Optional[General]:
         raise NotImplementedError
 
 class GeneralControl(GeneralControlInterface):
@@ -13,7 +13,7 @@ class GeneralControl(GeneralControlInterface):
     def __init__(self, syscmd: SystemCommandInterface=None):
         self._syscmd = syscmd or SystemCommand()
 
-    def __call__(self) -> List[General]:
+    def __call__(self) -> Optional[General]:
         r = self._syscmd.nmcli('general')
         row = r.split('\n')[1]
         m = re.search(r'^(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s*', row)
