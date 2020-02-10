8918d99ef40a9ad585e89447668145b7703dff5a
ushiboy <ushiboy.dev@gmail.com>
2020-02-10 09:50:51 +0900

    make public general

--- nmcli/__init__.py
+++ nmcli/__init__.py
@@ -1,5 +1,7 @@
 from ._device import DeviceControl
+from ._general import GeneralControl
 from ._system import SystemCommand
 
 _syscmd = SystemCommand()
 device = DeviceControl(_syscmd)
+general = GeneralControl(_syscmd)
