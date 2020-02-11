8762bc274b624bb5a97e4392afddbe6ea5f47618
ushiboy <ushiboy.dev@gmail.com>
2020-02-11 16:52:05 +0900

    fix lint

--- nmcli/_general.py
+++ nmcli/_general.py
@@ -1,22 +1,25 @@
-from ._system import SystemCommandInterface, SystemCommand
-from .data import General
 from typing import Optional
 import re
+from ._system import SystemCommandInterface, SystemCommand
+from .data import General
 
-class GeneralControlInterface(object):
+
+class GeneralControlInterface:
 
     def __call__(self) -> Optional[General]:
         raise NotImplementedError
 
+
 class GeneralControl(GeneralControlInterface):
 
-    def __init__(self, syscmd: SystemCommandInterface=None):
+    def __init__(self, syscmd: SystemCommandInterface = None):
         self._syscmd = syscmd or SystemCommand()
 
     def __call__(self) -> Optional[General]:
         r = self._syscmd.nmcli('general')
         row = r.split('\n')[1]
-        m = re.search(r'^(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s*', row)
+        m = re.search(
+            r'^(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s*', row)
         if m:
             state, connectivity, wifi_hw, wifi, wwan_hw, wwan = m.groups()
             return General(state, connectivity, wifi_hw, wifi, wwan_hw, wwan)
