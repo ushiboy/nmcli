dc3b63e3d1c6655c00b8b8ed5d4127831988e916
ushiboy <ushiboy.dev@gmail.com>
2020-02-15 12:08:27 +0900

    public data class

--- nmcli/__init__.py
+++ nmcli/__init__.py
@@ -2,6 +2,7 @@ from ._connection import ConnectionControl
 from ._device import DeviceControl
 from ._general import GeneralControl
 from ._system import SystemCommand
+from .data import Connection, Device, DeviceWifi, General
 
 _syscmd = SystemCommand()
 connection = ConnectionControl(_syscmd)
