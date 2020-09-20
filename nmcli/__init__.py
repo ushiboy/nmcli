1718acd320c4cced427e5f7bcf41fed402d157c8
ushiboy <ushiboy.dev@gmail.com>
2020-09-20 23:24:50 +0900

    add radio api

--- nmcli/__init__.py
+++ nmcli/__init__.py
@@ -12,11 +12,13 @@ from ._exception import UnspecifiedException, \
     NotExistException
 from ._general import GeneralControlInterface, GeneralControl
 from ._networking import NetworkingControlInterface, NetworkingControl
+from ._radio import RadioControlInterface, RadioControl
 from ._system import SystemCommand
-from .data import Connection, Device, DeviceWifi, General
+from .data import Connection, Device, DeviceWifi, General, Radio
 
 _syscmd = SystemCommand()
 connection = ConnectionControl(_syscmd)
 device = DeviceControl(_syscmd)
 general = GeneralControl(_syscmd)
 networking = NetworkingControl(_syscmd)
+radio = RadioControl(_syscmd)
