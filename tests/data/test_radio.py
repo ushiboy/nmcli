e79d1f2feefd73144e4431746637e68c908fc73c
ushiboy <ushiboy.dev@gmail.com>
2020-09-21 09:21:45 +0900

    add radio test

---
+++ tests/data/test_radio.py
@@ -0,0 +1,14 @@
+from nmcli.data import Radio
+
+def test_to_json():
+    wifi_hw = True
+    wifi = True
+    wwan_hw = True
+    wwan = True
+    r = Radio(wifi_hw, wifi, wwan_hw, wwan)
+    assert r.to_json() == {
+        'wifi_hw':wifi_hw,
+        'wifi':wifi,
+        'wwan_hw':wwan_hw,
+        'wwan':wwan
+    }
