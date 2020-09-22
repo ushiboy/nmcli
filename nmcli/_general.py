5cff0988ad836a9d0d0843bf6e0c262bce9c0a12
ushiboy <ushiboy.dev@gmail.com>
2020-09-22 14:19:01 +0900

    add get_hostname, set_hostname api

--- nmcli/_general.py
+++ nmcli/_general.py
@@ -9,6 +9,12 @@ class GeneralControlInterface:
     def status(self) -> General:
         raise NotImplementedError
 
+    def get_hostname(self) -> str:
+        raise NotImplementedError
+
+    def set_hostname(self, hostname: str):
+        raise NotImplementedError
+
 class GeneralControl(GeneralControlInterface):
 
     def __init__(self, syscmd: SystemCommandInterface = None):
@@ -20,3 +26,10 @@ class GeneralControl(GeneralControlInterface):
     def status(self) -> General:
         r = self._syscmd.nmcli(['general', 'status'])
         return General.parse(r.split('\n')[1])
+
+    def get_hostname(self) -> str:
+        r = self._syscmd.nmcli(['general', 'hostname'])
+        return r.replace('\n', '')
+
+    def set_hostname(self, hostname: str):
+        self._syscmd.nmcli(['general', 'hostname', hostname])
--- tests/test_general.py
+++ tests/test_general.py
@@ -12,7 +12,7 @@ connected  full          enabled  enabled  enabled  enabled''')
                         NetworkConnectivity.FULL, True, True, True, True)
     assert s.passed_parameters == ['general', 'status']
 
-def test_general_status():
+def test_status():
     s = DummySystemCommand('''STATE      CONNECTIVITY  WIFI-HW  WIFI     WWAN-HW  WWAN
 connected  full          enabled  enabled  enabled  enabled''')
     general = GeneralControl(s)
@@ -20,3 +20,15 @@ connected  full          enabled  enabled  enabled  enabled''')
     assert r == General(NetworkManagerState.CONNECTED_GLOBAL,
                         NetworkConnectivity.FULL, True, True, True, True)
     assert s.passed_parameters == ['general', 'status']
+
+def test_get_hostname():
+    s = DummySystemCommand('test\n')
+    general = GeneralControl(s)
+    assert general.get_hostname() == 'test'
+    assert s.passed_parameters == ['general', 'hostname']
+
+def test_set_hostname():
+    s = DummySystemCommand()
+    general = GeneralControl(s)
+    general.set_hostname('test')
+    assert s.passed_parameters == ['general', 'hostname', 'test']
