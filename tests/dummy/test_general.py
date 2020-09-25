8fdbd6c4fd3efb80c0cc8538732adcf99aaf6649
ushiboy <ushiboy.dev@gmail.com>
2020-09-25 23:48:54 +0900

    add dummy general test

--- tests/dummy/test_general.py
+++ tests/dummy/test_general.py
@@ -1,9 +1,12 @@
 import pytest
 from nmcli.dummy._general import DummyGeneralControl
 from nmcli.data import General
+from nmcli._const import NetworkManagerState, NetworkConnectivity
 
 def test_call():
-    result_call = General('connected', 'full', 'enabled', 'enabled', 'enabled', 'enabled')
+    result_call = General(NetworkManagerState.CONNECTED_GLOBAL,
+                          NetworkConnectivity.FULL,
+                          True, True, True, True)
     c = DummyGeneralControl(result_call)
     assert c() == result_call
 
@@ -16,3 +19,41 @@ def test_call_when_no_arguments_are_passed():
     c = DummyGeneralControl()
     with pytest.raises(ValueError):
         c()
+
+def test_status():
+    result_call = General(NetworkManagerState.CONNECTED_GLOBAL,
+                          NetworkConnectivity.FULL,
+                          True, True, True, True)
+    c = DummyGeneralControl(result_call)
+    assert c.status() == result_call
+
+def test_status_when_raise_error():
+    c = DummyGeneralControl(raise_error=Exception)
+    with pytest.raises(Exception):
+        c.status()
+
+def test_status_when_no_arguments_are_passed():
+    c = DummyGeneralControl()
+    with pytest.raises(ValueError):
+        c.status()
+
+def test_get_hostname():
+    result_hostname = 'test'
+    c = DummyGeneralControl(result_hostname=result_hostname)
+    assert c.get_hostname() == result_hostname
+
+def test_get_hostname_when_raise_error():
+    c = DummyGeneralControl(raise_error=Exception)
+    with pytest.raises(Exception):
+        c.get_hostname()
+
+def test_set_hostname():
+    hostname = 'test'
+    c = DummyGeneralControl()
+    c.set_hostname(hostname)
+    assert c.set_hostname_args == [hostname]
+
+def test_set_hostname_when_raise_error():
+    c = DummyGeneralControl(raise_error=Exception)
+    with pytest.raises(Exception):
+        c.set_hostname('')
