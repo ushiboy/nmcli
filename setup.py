087aba0d8a79831b3bdca5baf5cbbf941761153c
ushiboy <ushiboy.dev@gmail.com>
2020-10-03 15:13:11 +0900

    change version and module properties

--- setup.py
+++ setup.py
@@ -8,19 +8,31 @@ class PyTest(test):
         import pytest
         pytest.main(self.test_args)
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
 setup(
     name='nmcli',
-    version='0.2.2',
+    version='0.3.0',
+    author='ushiboy',
+    license='MIT',
+    description='A python wrapper library for the network-manager cli client',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/ushiboy/nmcli',
     packages=['nmcli', 'nmcli.data', 'nmcli.dummy'],
     package_data={
         'nmcli': ['py.typed'],
     },
     test_suite='tests',
     python_requires='>=3.7',
-    install_requires=[
-    ],
     tests_require=[
         'pytest'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux'
     ],
     cmdclass={'test': PyTest}
 )
