c5256593acf3e0ba3524c3d3d349dd74b9e7a9fb
ushiboy <ushiboy.dev@gmail.com>
2020-12-18 22:29:46 +0900

    fixed device wifi parse

--- nmcli/_device.py
+++ nmcli/_device.py
@@ -92,7 +92,8 @@ class DeviceControl(DeviceControlInterface):
         r = self._syscmd.nmcli(['device', 'wifi'])
         results = []
         for row in r.split('\n')[1:]:
-            results.append(DeviceWifi.parse(row))
+            if len(row) > 0:
+                results.append(DeviceWifi.parse(row))
         return results
 
     def wifi_connect(self, ssid: str, password: str) -> None:
