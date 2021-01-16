c8ce5e79fa285c420fb6068c6810fd469cd1b350
ushiboy <ushiboy.dev@gmail.com>
2021-01-16 15:03:07 +0900

    add parse include bssid line

--- tests/data/test_device.py
+++ tests/data/test_device.py
@@ -31,3 +31,19 @@ def test_device_wifi_to_json():
         'signal': signal,
         'security': security
     }
+
+def test_device_wifi_parse():
+    d1 = '*       AP1  Infra  1     130 Mbit/s  82      ______  WPA1 WPA2'
+    assert DeviceWifi.parse(d1) == \
+        DeviceWifi(True, 'AP1', 'Infra', 1, 130, 82, 'WPA1 WPA2')
+    d2 = '        AP1  Infra  1     130 Mbit/s  82      ______  WPA1 WPA2'
+    assert DeviceWifi.parse(d2) == \
+        DeviceWifi(False, 'AP1', 'Infra', 1, 130, 82, 'WPA1 WPA2')
+
+def test_device_wifi_parse_include_bssid_line():
+    d1 = '*       00:00:00:00:00:00  AP1  Infra  1     130 Mbit/s  82      ______  WPA1 WPA2'
+    assert DeviceWifi.parse_include_bssid_line(d1) == \
+        DeviceWifi(True, 'AP1', 'Infra', 1, 130, 82, 'WPA1 WPA2')
+    d2 = '        00:00:00:00:00:00  AP1  Infra  1     130 Mbit/s  82      ______  WPA1 WPA2'
+    assert DeviceWifi.parse_include_bssid_line(d2) == \
+        DeviceWifi(False, 'AP1', 'Infra', 1, 130, 82, 'WPA1 WPA2')
