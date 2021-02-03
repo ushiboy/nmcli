ff3290b7f00a0ed71e8a6afb3f6d30b97ec79439
ushiboy <ushiboy.dev@gmail.com>
2021-02-04 08:49:54 +0900

    version up v0.4.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='nmcli',
-    version='0.4.1',
+    version='0.4.2',
     author='ushiboy',
     license='MIT',
     description='A python wrapper library for the network-manager cli client',
