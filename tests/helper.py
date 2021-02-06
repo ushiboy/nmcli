5b0d00040a4e02d7719dc928000faf0f2c13671f
ushiboy <ushiboy.dev@gmail.com>
2021-02-06 13:33:43 +0900

    add parameters_history

--- tests/helper.py
+++ tests/helper.py
@@ -6,6 +6,10 @@ class DummySystemCommand(SystemCommandInterface):
     def passed_parameters(self):
         return self._parameters
 
+    @property
+    def parameters_history(self):
+        return self._parameters_history
+
     def __init__(self, result=None, raise_error=None):
         result = [] if result is None else result
         if isinstance(result, str):
@@ -15,9 +19,11 @@ class DummySystemCommand(SystemCommandInterface):
         self._result = result
         self._raise_error = raise_error
         self._parameters = []
+        self._parameters_history = []
 
     def nmcli(self, parameters):
         self._parameters = parameters
+        self._parameters_history.append(parameters)
         if not self._raise_error is None:
             raise self._raise_error
         if len(self._result) > 0:
