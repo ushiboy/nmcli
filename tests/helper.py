860c639568eca9c6f9e82a0af0fc3d01394ffb1b
ushiboy <ushiboy.dev@gmail.com>
2021-02-06 10:27:47 +0900

    fix helper

--- tests/helper.py
+++ tests/helper.py
@@ -6,12 +6,20 @@ class DummySystemCommand(SystemCommandInterface):
     def passed_parameters(self):
         return self._parameters
 
-    def __init__(self, result='', raise_error=None):
+    def __init__(self, result=None, raise_error=None):
+        result = [] if result is None else result
+        if isinstance(result, str):
+            result = [result]
+        if not isinstance(result, list):
+            raise ValueError('The result must be specified as list or str')
         self._result = result
         self._raise_error = raise_error
+        self._parameters = []
 
     def nmcli(self, parameters):
         self._parameters = parameters
         if not self._raise_error is None:
             raise self._raise_error
-        return self._result
+        if len(self._result) > 0:
+            return self._result.pop(0)
+        return ''
