9ec3475fa303507cf09d2b2ed932c1cc7eaf8209
ushiboy <ushiboy.dev@gmail.com>
2021-12-26 10:20:02 +0900

    fix lint

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -4,5 +4,5 @@ from nmcli._helper import add_wait_option_if_needed
 def test_add_wait_option_if_needed():
     assert add_wait_option_if_needed(10) == ['--wait', '10']
     assert add_wait_option_if_needed(11) == ['--wait', '11']
-    assert add_wait_option_if_needed() == []
-    assert add_wait_option_if_needed(None) == []
+    assert not add_wait_option_if_needed()
+    assert not add_wait_option_if_needed(None)
