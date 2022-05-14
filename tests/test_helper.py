93dfbc7135fddce8f1bce2bebce414f32e7ef864
ushiboy <ushiboy.dev@gmail.com>
2022-05-14 10:39:59 +0900

    added test when passing fields option

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,4 +1,4 @@
-from nmcli._helper import add_wait_option_if_needed
+from nmcli._helper import add_wait_option_if_needed, add_fields_option_if_needed
 
 
 def test_add_wait_option_if_needed():
@@ -6,3 +6,9 @@ def test_add_wait_option_if_needed():
     assert add_wait_option_if_needed(11) == ['--wait', '11']
     assert not add_wait_option_if_needed()
     assert not add_wait_option_if_needed(None)
+
+def test_add_fields_option_if_needed():
+    assert add_fields_option_if_needed('all') == ['-f', 'all']
+    assert add_fields_option_if_needed('common') == ['-f', 'common']
+    assert not add_fields_option_if_needed()
+    assert not add_fields_option_if_needed(None)
