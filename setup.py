054e1a7c4091998d3e734ceec9cc3b61bab87621
ushiboy <ushiboy.dev@gmail.com>
2022-12-17 10:22:25 +0900

    version up 1.1.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='nmcli',
-    version='1.1.1',
+    version='1.1.2',
     author='ushiboy',
     license='MIT',
     license_files = ('LICENSE.txt',),
