c77a40fc9ddbd7958d0c7c9909a4f9f2631f51ef
ushiboy <ushiboy.dev@gmail.com>
2025-12-13 22:40:38 +0900

    Migrate setup.py configuration to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,39 +1,6 @@
 #!/usr/bin/env python3
+# This file is kept for backwards compatibility.
+# All configuration is now in pyproject.toml
 from setuptools import setup
-from setuptools.command.test import test
 
-class PyTest(test):
-
-    def run_tests(self):
-        import pytest
-        pytest.main(self.test_args)
-
-with open('README.md', 'r') as f:
-    long_description = f.read()
-
-setup(
-    name='nmcli',
-    version='1.5.0',
-    author='ushiboy',
-    license='MIT',
-    license_files = ('LICENSE.txt',),
-    description='A python wrapper library for the network-manager cli client',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/ushiboy/nmcli',
-    packages=['nmcli', 'nmcli.data', 'nmcli.dummy'],
-    package_data={
-        'nmcli': ['py.typed'],
-    },
-    test_suite='tests',
-    python_requires='>=3.7',
-    tests_require=[
-        'pytest'
-    ],
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: POSIX :: Linux'
-    ],
-    cmdclass={'test': PyTest}
-)
+setup()
